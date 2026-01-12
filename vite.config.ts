57b811210aa4be11d5b0383b8e3ebd6783c98c50
asdf <asdf@gmail.com>
2026-01-12 22:04:15 +0530

    Update base path to /parimeterx/

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/breach-aware/',
+  base: '/parimeterx/',
   server: {
     host: "::",
     port: 8080,
